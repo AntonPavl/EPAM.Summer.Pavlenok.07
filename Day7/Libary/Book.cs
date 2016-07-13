d18756443e24345d4d4461672a99e8e5aa084b30
AntonPavl <pavlenok.anton@gmail.com>
2016-07-13 23:03:25 +0300

    TempStr

--- Day7/Libary/Book.cs
+++ Day7/Libary/Book.cs
@@ -6,7 +6,79 @@ using System.Threading.Tasks;
 
 namespace Libary
 {
-    public class Book
+    public class Book : IEquatable<Book>, IComparable<Book>
     {
+        private string author;
+        private string title;
+        private int id;
+        private int pages;
+
+        public string Author
+        {
+            get
+            {
+                return author;
+            }
+
+            set
+            {
+                author = value;
+            }
+        }
+
+        public string Title
+        {
+            get
+            {
+                return title;
+            }
+
+            set
+            {
+                title = value;
+            }
+        }
+
+        public int Id
+        {
+            get
+            {
+                return id;
+            }
+
+            set
+            {
+                id = value;
+            }
+        }
+
+        public int Pages
+        {
+            get
+            {
+                return pages;
+            }
+
+            set
+            {
+                pages = value;
+            }
+        }
+
+        public int CompareTo(Book other)
+        {
+            if (this.)
+        }
+
+        public bool Equals(Book other)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override int GetHashCode()
+        {
+            return base.GetHashCode();
+        }
     }
+
 }
